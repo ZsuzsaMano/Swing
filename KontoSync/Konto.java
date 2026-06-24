269fa5945b73695ef8ad24c03d7e7f83235291ee
Zsuzsa.L <manoaway@gmail.com>
2026-06-24 22:24:26 +0200

    corrections for Konto sync

--- KontoSync/Konto.java
+++ KontoSync/Konto.java
@@ -1,16 +1,26 @@
 package KontoSync;
 
 public class Konto {
-    private int saldo;
+    private volatile int saldo; // to make sure, print always has the right value
 
     public Konto(int saldo) {
         this.saldo = saldo;
     }
 
-    public void einzahlen(int betrag) {
+    public synchronized void einzahlen(int betrag) {
         int temp = saldo + betrag;
         // Thread.yield(); //force interrupt thread to simulate possible error source
         saldo = temp;
+    }
+
+    // public void einzahlen(int betrag) {
+    // int temp = saldo + betrag;
+    // Thread.yield(); //force interrupt thread to simulate possible error source
+    // saldo = temp;
+    // }
+
+    public synchronized void auszahlen(int betrag) { // muss auch Synchronized sein
+        saldo = saldo - betrag;
     }
 
     public void printSaldo() {
